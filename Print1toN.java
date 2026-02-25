d2da93a601e579bcd7748893501b62238b150bed
Megha706765 <meghathakur2003123@gmail.com>
2026-02-25 11:35:04 +0530

    UseForLoop

---
+++ Print1toN.java
@@ -0,0 +1,11 @@
+
+class Print1toN {
+
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i = 1; i <= n; i++) {
+
+            System.out.print(i + " ");
+        }
+    }
+}
