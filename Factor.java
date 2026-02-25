5d7205cdbedc622a6cac43fae771accfd228588d
Megha706765 <meghathakur2003123@gmail.com>
2026-02-25 12:13:09 +0530

    UseForLoop

---
+++ Factor.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+class Factor {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int fac = 1;
+        for (int i = 1; i <= n; i++) {
+            fac = fac * i;
+        }
+        System.out.print(fac);
+    }
+}
